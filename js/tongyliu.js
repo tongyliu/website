5cc451ee0624ce5eefe4e0a2d7526aa942c84d74
Tong Liu <tongyliu@gmail.com>
2016-01-12 19:53:33 -0500

    Fix trapezoid overflow on iOS devices & add shadows

--- js/tongyliu.js
+++ js/tongyliu.js
@@ -5,6 +5,25 @@
  * Tong Liu, Jan 2016
  */
 
-(function() {
+$(function() {
 	new WOW().init();
-})();
+
+	// Prevent any overflow-x from showing
+	$('.wrapper').scroll(function() {
+		if ($(document).scrollLeft()) {
+			$('.wrapper').scrollLeft(0);
+		} 
+	});
+
+	// iOS devices seem to require a clip to stop the .trapezoid-wrapper
+	// from showing overflow	
+	if (isAppleDevice()) {
+		$('.trapezoid-wrapper').css(
+			'-webkit-clip-path', 'polygon(0 0, 100% 0%, 100% 100%, 0 100%)'
+		);
+	}
+});
+
+function isAppleDevice() {
+	return navigator.userAgent.match(/iPad|iPhone|iPod/i) !== null;
+}
