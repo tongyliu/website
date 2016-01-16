d392d9cc049e398237f80d62b3d6351885a9d5b4
Tong Liu <tongyliu@gmail.com>
2016-01-15 23:22:01 -0500

    Make site responsive

--- js/tongyliu.js
+++ js/tongyliu.js
@@ -5,6 +5,13 @@
  * Tong Liu, Jan 2016
  */
 
-(function() {
-	new WOW().init();
-})();
+$(function() {
+	new WOW({ mobile: false }).init();
+	if (isMobileDevice()) {
+		$('body').addClass('fixed-bg');
+	}
+});
+
+function isMobileDevice() {
+	return (navigator.userAgent.match(/Android|iPad|iPhone|iPod/i) !== null);
+}
