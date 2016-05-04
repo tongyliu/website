d4dceb30b8347d4157f26fd778bc7a0107adafb6
Tong Liu <tongyliu@gmail.com>
2016-05-04 13:50:51 -0400

    Use PRG pattern to prevent contact form resubmissions

--- v2/contact.php
+++ v2/contact.php
@@ -2,10 +2,12 @@
 
 // Again not the real file.
 
+session_start();
+
 if (isset($_POST['name'], $_POST['email'], $_POST['msg'])) {
-	$should_render_form = false;
-} else {
-	$should_render_form = true;
+	$_SESSION['contact_request_status'] = rand(0, 1);
+	header('Location: contacted');
+	exit();
 }
 
 ?>
@@ -23,27 +25,21 @@ if (isset($_POST['name'], $_POST['email'], $_POST['msg'])) {
 		<div class="section-heading">
 			<h2>Contact</h2>
 		</div>
-		<?php if ($should_render_form): ?>
-			<form id="contact-form" action="contact.php" method="POST" novalidate>
-				<div class="form-field">
-					<label class="required" for="name-input">Name</label>
-					<input id="name-input" type="text" name="name"/>
-				</div>
-				<div class="form-field">
-					<label class="required" for="email-input">Email</label>
-					<input id="email-input" type="email" name="email"/>
-				</div>
-				<div class="form-field form-field--textarea">
-					<label class="required" for="msg-input">Message</label>
-					<textarea id="msg-input" rows="1" name="msg"></textarea>
-				</div>
-				<button class="button button--disabled" type="submit" disabled>Send</button>
-			</form>
-		<?php else: ?>
-			<div class="confirmation">
-				Thanks for reaching out! I'll get back to you as soon as I can.
+		<form id="contact-form" action="contact.php" method="POST" novalidate>
+			<div class="form-field">
+				<label class="required" for="name-input">Name</label>
+				<input id="name-input" type="text" name="name"/>
 			</div>
-		<?php endif; ?>
+			<div class="form-field">
+				<label class="required" for="email-input">Email</label>
+				<input id="email-input" type="email" name="email"/>
+			</div>
+			<div class="form-field form-field--textarea">
+				<label class="required" for="msg-input">Message</label>
+				<textarea id="msg-input" rows="1" name="msg"></textarea>
+			</div>
+			<button class="button button--disabled" type="submit" disabled>Send</button>
+		</form>
 		<div class="profile-buttons">
 			<a href="https://github.com/tongyliu">
 				<div class="profile-button profile-button--github"></div>
@@ -52,15 +48,10 @@ if (isset($_POST['name'], $_POST['email'], $_POST['msg'])) {
 				<div class="profile-button profile-button--linkedin"></div>
 			</a>
 		</div>
-		<?php if (!$should_render_form): ?>
-			<a class="button button--small back-button" href="index">Back</a></li>
-		<?php endif; ?>
 	</div>
-	<?php if ($should_render_form): ?>
-		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.11.2/lodash.min.js"></script>
-		<script type="text/javascript" src="js/autosize.min.js"></script>
-		<script type="text/javascript" src="js/contact.js"></script>
-	<?php endif; ?>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.11.2/lodash.min.js"></script>
+	<script type="text/javascript" src="js/autosize.min.js"></script>
+	<script type="text/javascript" src="js/contact.js"></script>
 </body>
 </html>
