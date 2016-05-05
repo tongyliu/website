2194510dc6d519f393eeb330a961fd6914705adf
Tong Liu <tongyliu@gmail.com>
2016-05-04 23:45:17 -0400

    Update .gitignore, gulpfile, and favicon

--- v2/gulpfile.js
+++ v2/gulpfile.js
@@ -1,6 +1,11 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefix = require('gulp-autoprefixer');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var source = require('vinyl-source-stream');
  
 gulp.task('css', function() {
   gulp.src('css/**/*.scss')
@@ -9,8 +14,25 @@ gulp.task('css', function() {
     .pipe(gulp.dest('css/'));
 });
 
-gulp.task('watch', ['css'], function() {
-  gulp.watch('css/**/*.scss', ['css']);
+gulp.task('js', function() {
+  browserify(['js/main.js'])
+    .transform(babelify, { presets: ['react'] })
+    .bundle()
+    .on('error', function(e) { console.log(e.message); this.emit('end'); })
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('js/'));
 });
 
-gulp.task('default', ['css']);
+gulp.task('minify-js', ['js'], function() {
+  gulp.src('js/bundle.js')
+    .pipe(uglify())
+    .pipe(rename('bundle.min.js'))
+    .pipe(gulp.dest('js/'));
+});
+
+gulp.task('watch', ['css'], function() {
+  gulp.watch('css/**/*.scss', ['css']);
+  gulp.watch('js/main.js', ['js']);
+});
+
+gulp.task('default', ['css', 'js']);
