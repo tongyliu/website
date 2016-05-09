b11b182c15df09a1f152a0904d50d0ff38f388f1
Tong Liu <tongyliu@gmail.com>
2016-05-09 11:53:43 -0400

    Use endashes in titles

--- v2/contact.php
+++ v2/contact.php
@@ -15,7 +15,7 @@ if (isset($_POST['name'], $_POST['email'], $_POST['msg'])) {
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Tong Liu - Contact</title>
+	<title>Tong Liu &ndash; Contact</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="icon" type="image/x-icon" href="favicon.ico"/>
 	<link rel="stylesheet" type="text/css" href="css/styles.css"/>
